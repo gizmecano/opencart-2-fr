98953eed5301e259f02c2e2dd5e02673a69ad3ff
P. Mergey <gizmecano+github@gmail.com>
2015-08-31 07:46:21 +0200

    Minor adjustments in <catalog/affiliate>

--- catalog/language/fr-FR/affiliate/password.php
+++ catalog/language/fr-FR/affiliate/password.php
@@ -12,5 +12,5 @@ $_['entry_password'] = 'Mot de passe';
 $_['entry_confirm']  = 'Confirmer le mot de passe';
 
 // Error
-$_['error_password'] = 'Le mot de passe doit comporter entre 4 et 20 caractères !';
+$_['error_password'] = 'Le mot de passe doit contenir entre 4 et 20 caractères !';
 $_['error_confirm']  = 'La confirmation de mot de passe ne correspond pas au mot de passe saisi!';
