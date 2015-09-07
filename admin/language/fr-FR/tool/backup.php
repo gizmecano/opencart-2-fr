e5dc0b487a14c37c0f4ff089b9608a8ae17dd7c6
P. Mergey <gizmecano+github@gmail.com>
2015-09-07 12:25:50 +0200

    Add <admin/tool>

---
+++ admin/language/fr-FR/tool/backup.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']    = 'Sauvegarde et restauration';
+
+// Text
+$_['text_backup']      = 'Sauvegarder';
+$_['text_success']     = 'Succès: la base de données a été correctement sauvegardée!';
+$_['text_list']        = 'Liste des transferts';
+
+// Entry
+$_['entry_restore']    = 'Restaurer une sauvegarde';
+$_['entry_backup']     = 'Sauvegarde';
+
+// Error
+$_['error_permission'] = 'Attention: vous n\'êtes pas autorisé à modifier les sauvegardes!';
+$_['error_backup']     = 'Attention: vous devez sélectionner au moins une table à sauvegarder!';
+$_['error_empty']      = 'Attention: le fichier transféré est vide!';
---
+++ admin/language/fr-FR/tool/upload.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']     = 'Transfert de fichiers';
+
+// Text
+$_['text_success']      = 'Succès: vous avez modifié le fichier transféré!';
+$_['text_list']         = 'Liste des transferts';
+
+// Column
+$_['column_name']       = 'Nom du transfert';
+$_['column_filename']   = 'Nom du fichier';
+$_['column_date_added'] = 'Date d\'ajout';
+$_['column_action']     = 'Action';
+
+// Entry
+$_['entry_name']        = 'Nom du transfert';
+$_['entry_filename']    = 'Nom du fichier';
+$_['entry_date_added'] 	= 'Date d\'ajout';
+
+// Error
+$_['error_permission']  = 'Attention: vous n\'êtes pas autorisé à modifier les transferts de fichiers!';
