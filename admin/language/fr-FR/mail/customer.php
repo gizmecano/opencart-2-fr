186216373e5742810c4414e346cc1d17f3ab3a28
P. Mergey <gizmecano+github@gmail.com>
2015-09-07 12:20:45 +0200

    Add <admin/mail>

---
+++ admin/language/fr-FR/mail/customer.php
@@ -0,0 +1,13 @@
+<?php
+// Text
+$_['text_approve_subject']      = '%s - Votre compte a été activé !';
+$_['text_approve_welcome']      = 'Nous vous souhaitons la bienvenue et vous remercions de vous être enregistré sur %s !';
+$_['text_approve_login']        = 'Votre compte a été créé et vous pouvez vous connecter en utilisant votre adresse électronique et votre mot de passe sur notre site internet à l\'adresse suivante :';
+$_['text_approve_services']     = 'Lors de la connexion, vous serez en mesure d\'accéder à d\'autres services, tels que la visualisation de vos commandes passées, l\'impression de vos factures ou encore l\'édition de vos informations de compte.';
+$_['text_approve_thanks']       = 'Merci,';
+$_['text_transaction_subject']  = '%s - Compte créditeur';
+$_['text_transaction_received'] = 'Vous venez de recevoir %s crédit!';
+$_['text_transaction_total']    = 'Le montant total de votre crédit est %s.' . "\n\n" . 'Le montant de votre crédit sera automatiquement déduit de votre prochain achat.';
+$_['text_reward_subject']       = '%s - Points de fidélité';
+$_['text_reward_received']      = 'Vous venez de recevoir %s points de fidélité !';
+$_['text_reward_total']         = 'Votre nombre total de points de fidélité est de %s.';
