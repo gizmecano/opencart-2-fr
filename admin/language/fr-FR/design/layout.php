255913e65a414a817f71908b5a7b3f86ea4a09c4
P. Mergey <gizmecano+github@gmail.com>
2015-09-07 12:18:45 +0200

    Add <admin/design>

---
+++ admin/language/fr-FR/design/layout.php
@@ -0,0 +1,35 @@
+<?php
+// Heading
+$_['heading_title']       = 'Mises en page';
+
+// Text
+$_['text_success']        = 'Vous venez de modifier la mise en page !';
+$_['text_list']           = 'Liste des mises en page';
+$_['text_add']            = 'Ajouter une structure';
+$_['text_edit']           = 'Modifier une structure';
+$_['text_default']        = 'Par défaut';
+$_['text_content_top']    = 'Contenu du haut';
+$_['text_content_bottom'] = 'Contenu du bas';
+$_['text_column_left']    = 'Colonne de gauche';
+$_['text_column_right']   = 'Colonne de droite';
+
+// Column
+$_['column_name']         = 'Nom de la structure';
+$_['column_action']       = 'Action';
+
+// Entry
+$_['entry_name']          = 'Nom de la structure';
+$_['entry_store']         = 'Boutique';
+$_['entry_route']         = 'Chemin';
+$_['entry_module']        = 'Module';
+$_['entry_position']      = 'Position';
+$_['entry_sort_order']    = 'Classement';
+
+// Error
+$_['error_permission']    = 'Attention : vous n\'êtes pas autorisé à modifier les mises en page !';
+$_['error_name']          = 'Le nom de la structure doit contenir entre 3 et 64 caractères !';
+$_['error_default']       = 'Attention ! Cette structure ne peut être supprimée car elle est actuellement définie comme mise en page par défaut pour cette boutique !';
+$_['error_store']         = 'Attention ! Cette structure ne peut être supprimée car elle est actuellement affectée à %s boutiques !';
+$_['error_product']       = 'Attention ! Cette structure ne peut être supprimée car elle est actuellement affectée à %s produits !';
+$_['error_category']      = 'Attention ! Cette structure ne peut être supprimée car elle est actuellement affectée à %s catégories !';
+$_['error_information']   = 'Attention ! Cette structure ne peut être supprimée car elle est actuellement affectée à %s pages d\'informations !';
