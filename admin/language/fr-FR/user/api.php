ac9da1f724e0f987e805fba2c98108f499f81131
P. Mergey <gizmecano+github@gmail.com>
2015-09-08 09:55:10 +0200

    Add <admin/user>

---
+++ admin/language/fr-FR/user/api.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']        = 'Interfaces de programmation (API)';
+
+// Text
+$_['text_success']         = 'Succès: vous avez modifié les API!';
+$_['text_list']            = 'Liste des API';
+$_['text_add']             = 'Ajouter une API';
+$_['text_edit']            = 'Modifier les API';
+
+// Column
+$_['column_username']      = 'Nom d\'utilisateur';
+$_['column_status']        = 'Statut';
+$_['column_date_added']    = 'Date d\'ajout';
+$_['column_date_modified'] = 'Modifié le';
+$_['column_action']        = 'Action';
+
+// Entry
+$_['entry_username']       = 'Nom d\'utilisateur';
+$_['entry_password']       = 'Mot de passe';
+$_['entry_status']         = 'Statut';
+
+// Error
+$_['error_permission']     = 'Attention: vous n\'avez pas la permission de modifier les API!';
+$_['error_username']       = 'Le nom d\'utilisateur doit contenir entre 3 et 20 caractères!';
+$_['error_password']       = 'Le mot de passe de l\'API doit contenir entre 3 et 256 caractères!';
---
+++ admin/language/fr-FR/user/user.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['heading_title']     = 'Utilisateurs';
+
+// Text
+$_['text_success']      = 'Succès: vous avez modifié users!';
+$_['text_list']         = 'Liste d\'utilisateurs';
+$_['text_add']          = 'Ajouter un utilisateur';
+$_['text_edit']         = 'Modifier un utilisateur';
+
+// Column
+$_['column_username']   = 'Nom d\'utilisateur';
+$_['column_status']     = 'Statut';
+$_['column_date_added'] = 'Date d\'ajout';
+$_['column_action']     = 'Action';
+
+// Entry
+$_['entry_username']   	= 'Nom d\'utilisateur';
+$_['entry_user_group'] 	= 'groupe d\'utilisateurs';
+$_['entry_password']   	= 'Mot de passe';
+$_['entry_confirm']    	= 'Confirmer';
+$_['entry_firstname']  	= 'Prénom';
+$_['entry_lastname']   	= 'Nom de famille';
+$_['entry_email']      	= 'Adresse électronique';
+$_['entry_image']      	= 'Image';
+$_['entry_status']     	= 'Statut';
+
+// Error
+$_['error_permission'] 	= 'Attention: vous n\'avez pas la permission de modifier les utilisateurs!';
+$_['error_account']    	= 'Attention: vous ne pouvez pas supprimé votre propre compte!';
+$_['error_exists']     	= 'Attention: ce nom d\'utilisateur est déjà utilisé!';
+$_['error_username']   	= 'Le nom d\'utilisateur doit contenir entre 3 et 20 caractères!';
+$_['error_password']   	= 'Le mot de passe doit contenir entre 4 et 20 caractères !';
+$_['error_confirm']    	= 'Le mot de passe de confirmation ne correspond pas au premier mot de passe !';
+$_['error_firstname']  	= 'Le prénom doit contenir entre 1 à 32 caractères !';
+$_['error_lastname']   	= 'Le nom de famille doit contenir entre 1 à 32 caractères !';
