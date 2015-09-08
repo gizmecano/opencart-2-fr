8d0e30157ded7b0379f1bf060443cd5ef0e859e6
P. Mergey <gizmecano+github@gmail.com>
2015-09-08 09:50:06 +0200

    Add <admin/sale>

---
+++ admin/language/fr-FR/sale/custom_field.php
@@ -0,0 +1,49 @@
+<?php
+// Heading
+$_['heading_title']         = 'Champs personnalisés';
+
+// Text
+$_['text_success']          = 'Succès : vous avez modifié les champs personnalisés !';
+$_['text_list']             = 'Liste des champs personnalisés';
+$_['text_add']              = 'Ajouter un champ personnalisé';
+$_['text_edit']             = 'Modifier le champ personnalisé';
+$_['text_choose']           = 'Choisir';
+$_['text_select']           = 'Sélection';
+$_['text_radio']            = 'Radio';
+$_['text_checkbox']         = 'Case à cocher';
+$_['text_input']            = 'Entrée de données';
+$_['text_text']             = 'Texte';
+$_['text_textarea']         = 'Zone de texte';
+$_['text_file']             = 'Fichier';
+$_['text_date']             = 'Date';
+$_['text_datetime']         = 'Date et heure';
+$_['text_time']             = 'Heure';
+$_['text_account']          = 'Compte';
+$_['text_address']          = 'Adresse';
+
+// Column
+$_['column_name']           = 'Nom du champ personnalisé';
+$_['column_location']       = 'Emplacement';
+$_['column_type']           = 'Type';
+$_['column_sort_order']     = 'Classement';
+$_['column_action']         = 'Action';
+
+// Entry
+$_['entry_name']            = 'Nom du champ personnalisé';
+$_['entry_location']        = 'Emplacement';
+$_['entry_type']            = 'Type';
+$_['entry_value']           = 'Valeur';
+$_['entry_custom_value']    = 'Nom de valeur du champ personnalisé';
+$_['entry_customer_group']  = 'Groupe de clients';
+$_['entry_required']        = 'Champ obligatoire';
+$_['entry_status']          = 'Statut';
+$_['entry_sort_order']      = 'Classement';
+
+// Help
+$_['help_sort_order']       = 'Utiliser le signe moins pour revenir au champ précédent.';
+
+// Error
+$_['error_permission']      = 'Attention: vous n\'êtes pas autorisé à modifier les champs personnalisés !';
+$_['error_name']            = 'Le nom du champ personnalisé doit contenir entre 1 et 128 caractères !';
+$_['error_type']            = 'Attention: les valeurs du champ personnalisé sont requises !';
+$_['error_custom_value']    = 'Le nom du champ personnalisé doit contenir entre 1 et 128 caractères !';
---
+++ admin/language/fr-FR/sale/customer_ban_ip.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'IP bannies';
+
+// Text
+$_['text_success']     = 'Succès: vous avez modifié les IP bannies!';
+$_['text_list']        = 'Liste des IP bannies';
+$_['text_add']         = 'Ajouter une IP bannie';
+$_['text_edit']        = 'Modifier une IP bannie';
+
+// Column
+$_['column_ip']        = 'IP';
+$_['column_customer']  = 'Clients';
+$_['column_action']    = 'Action';
+
+// Entry
+$_['entry_ip']         = 'IP';
+
+// Error
+$_['error_permission'] = 'Attention: vous n\'avez pas la permission de modifier les IP bannies!';
+$_['error_ip']         = 'Une IP doit contenir entre 1 et 40 caractères!';
