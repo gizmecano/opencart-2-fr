90f43dfa0c5b265789be509f5813d9e87de82325
P. Mergey <gizmecano+github@gmail.com>
2015-09-08 08:30:11 +0200

    Add <admin/feed>

---
+++ admin/language/fr-FR/feed/google_base.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']    = 'Google Base';
+
+// Text
+$_['text_feed']        = 'Flux des produits';
+$_['text_success']     = 'Vous venez de modifier le flux Google Base !';
+$_['text_list']        = 'Liste des mises en page';
+$_['text_edit']        = 'Modifier Google Base';
+
+// Entry
+$_['entry_status']     = 'Statut';
+$_['entry_data_feed']  = 'URL du flux';
+
+// Error
+$_['error_permission'] = 'Attention ! Vous n\'êtes pas autorisé à modifier l\'URL du flux Google Base !';
---
+++ admin/language/fr-FR/feed/google_sitemap.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'Google Sitemap';
+
+// Text
+$_['text_feed']        = 'Flux des produits';
+$_['text_success']     = 'Vous venez de modifier le flux Google Sitemap !';
+$_['text_edit']        = 'Modifier Google Sitemap';
+
+// Entry
+$_['entry_status']     = 'Statut';
+$_['entry_data_feed']  = 'URL du flux';
+
+// Error
+$_['error_permission'] = 'Attention ! Vous n\'êtes pas autorisé à modifier l\'URL du flux Google Sitemap !';
