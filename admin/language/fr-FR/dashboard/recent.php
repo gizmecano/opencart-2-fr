e18377fbe29ff94fa6ccc6955dc3218740c01bad
P. Mergey <gizmecano+github@gmail.com>
2015-09-10 08:20:05 +0200

    Update <admin\dashboard>

--- admin/language/fr-FR/dashboard/recent.php
+++ admin/language/fr-FR/dashboard/recent.php
@@ -5,7 +5,7 @@ $_['heading_title']     = 'Dernières commandes';
 // Column
 $_['column_order_id']   = 'Commande n°';
 $_['column_customer']   = 'Client';
-$_['column_status']     = 'État';
+$_['column_status']     = 'Statut';
 $_['column_total']      = 'Total';
 $_['column_date_added'] = 'Date d\'ajout';
 $_['column_action']     = 'Action';
