b6a004a69a1b68ecec5d3818ef1dcd78afc07029
P. Mergey <gizmecano+github@gmail.com>
2015-09-10 08:24:05 +0200

    Update <admin\mail>

--- admin/language/fr-FR/mail/return.php
+++ admin/language/fr-FR/mail/return.php
@@ -3,6 +3,6 @@
 $_['text_subject']       = '%s - Mise à jour du retour %s';
 $_['text_return_id']     = 'Numéro de retour:';
 $_['text_date_added']    = 'Date de retour :';
-$_['text_return_status'] = 'Votre retour a été mis à l\'état suivant:';
+$_['text_return_status'] = 'Votre retour a été mis au statut suivant:';
 $_['text_comment']       = 'Les commentaires pour ce retour sont :';
 $_['text_footer']        = 'Veuillez répondre à ce message si vous avez des questions.';
