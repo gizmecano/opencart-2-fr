b221509dbcbfa82eecc711c3be6476c922369379
P. Mergey <gizmecano+github@gmail.com>
2015-09-18 08:15:30 +0200

    Revise translations in <catalog\payment>

--- catalog/language/fr-FR/payment/cheque.php
+++ catalog/language/fr-FR/payment/cheque.php
@@ -3,5 +3,5 @@
 $_['text_title']				= 'Chèque ou mandat';
 $_['text_instruction']			= 'Instructions de paiement';
 $_['text_payable']				= 'Payable à :';
-$_['text_address']				= 'Envoyé à: ';
+$_['text_address']				= 'À envoyer à: ';
 $_['text_payment']				= 'Votre commande sera envoyée après réception de votre paiement.';
