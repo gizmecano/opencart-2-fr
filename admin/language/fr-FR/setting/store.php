5caf51284db44c7533c096fba76225a83f7d6ec3
P. Mergey <gizmecano+github@gmail.com>
2015-09-21 07:16:45 +0200

    Revise translations in <admin\setting>

--- admin/language/fr-FR/setting/store.php
+++ admin/language/fr-FR/setting/store.php
@@ -121,4 +121,4 @@ $_['error_image_wishlist']             = 'Taille des images des produits de la l
 $_['error_image_cart']                 = 'Taille des images des produits dans le panier requise!';
 $_['error_image_location']             = 'Taille des images des produits de la boutique requise!';
 $_['error_default']                    = 'Attention: vous ne pouvez pas supprimer votre boutique par défaut!';
-$_['error_store']                      = 'Attention: cette boutique ne peut pas être supprimée car elle est actuellement assignée à   %s commandes!';
+$_['error_store']                      = 'Attention: cette boutique ne peut pas être supprimée car elle est actuellement attribuée à   %s commandes!';
