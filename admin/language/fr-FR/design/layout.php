f8db40acb007bc9a68b916538fdcaa856f723b38
P. Mergey <gizmecano+github@gmail.com>
2015-09-22 10:07:05 +0200

    Revise translations in <admin\design>

--- admin/language/fr-FR/design/layout.php
+++ admin/language/fr-FR/design/layout.php
@@ -1,12 +1,12 @@
 <?php
 // Heading
-$_['heading_title']       = 'Mises en page';
+$_['heading_title']       = 'Gabarits';
 
 // Text
-$_['text_success']        = 'Vous venez de modifier la mise en page !';
-$_['text_list']           = 'Liste des mises en page';
-$_['text_add']            = 'Ajouter une structure';
-$_['text_edit']           = 'Modifier une structure';
+$_['text_success']        = 'Vous venez de modifier les gabarits !';
+$_['text_list']           = 'Liste des gabarits';
+$_['text_add']            = 'Ajouter un gabarit';
+$_['text_edit']           = 'Modifier un gabarit';
 $_['text_default']        = 'Par défaut';
 $_['text_content_top']    = 'Contenu du haut';
 $_['text_content_bottom'] = 'Contenu du bas';
@@ -14,11 +14,11 @@ $_['text_column_left']    = 'Colonne de gauche';
 $_['text_column_right']   = 'Colonne de droite';
 
 // Column
-$_['column_name']         = 'Nom de la structure';
+$_['column_name']         = 'Nom du gabarit';
 $_['column_action']       = 'Action';
 
 // Entry
-$_['entry_name']          = 'Nom de la structure';
+$_['entry_name']          = 'Nom du gabarit';
 $_['entry_store']         = 'Boutique';
 $_['entry_route']         = 'Chemin';
 $_['entry_module']        = 'Module';
@@ -26,10 +26,10 @@ $_['entry_position']      = 'Position';
 $_['entry_sort_order']    = 'Classement';
 
 // Error
-$_['error_permission']    = 'Attention : vous n\'êtes pas autorisé à modifier les mises en page !';
-$_['error_name']          = 'Le nom de la structure doit contenir entre 3 et 64 caractères !';
-$_['error_default']       = 'Attention ! Cette structure ne peut être supprimée car elle est actuellement définie comme mise en page par défaut pour cette boutique !';
-$_['error_store']         = 'Attention ! Cette structure ne peut être supprimée car elle est actuellement attribuée à %s boutiques !';
-$_['error_product']       = 'Attention ! Cette structure ne peut être supprimée car elle est actuellement attribuée à %s produits !';
-$_['error_category']      = 'Attention ! Cette structure ne peut être supprimée car elle est actuellement attribuée à %s catégories !';
-$_['error_information']   = 'Attention ! Cette structure ne peut être supprimée car elle est actuellement attribuée à %s pages d\'informations !';
+$_['error_permission']    = 'Attention : vous n\'êtes pas autorisé à modifier les gabarits !';
+$_['error_name']          = 'Le nom du gabarit doit contenir entre 3 et 64 caractères !';
+$_['error_default']       = 'Attention ! Ce gabarit ne peut être supprimée car il est actuellement défini comme gabarit par défaut pour cette boutique !';
+$_['error_store']         = 'Attention ! Ce gabarit ne peut être supprimé car il est actuellement attribué à %s boutiques !';
+$_['error_product']       = 'Attention ! Ce gabarit ne peut être supprimé car il est actuellement attribué à %s produits !';
+$_['error_category']      = 'Attention ! Ce gabarit ne peut être supprimé car il est actuellement attribué à %s catégories !';
+$_['error_information']   = 'Attention ! Ce gabarit ne peut être supprimé car il est actuellement attribué à %s pages d\'informations !';
