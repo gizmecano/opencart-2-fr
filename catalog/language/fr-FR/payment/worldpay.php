de5bad103e898aa469e1b5b7b5c48266277dd3f6
P. Mergey <gizmecano+github@gmail.com>
2016-12-09 14:25:10 +0100

    Revise files in `catalog/payment`

--- catalog/language/fr-FR/payment/worldpay.php
+++ catalog/language/fr-FR/payment/worldpay.php
@@ -13,13 +13,27 @@
 *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 
-// Heading
-$_['heading_title'] = 'Merci de faire vos achats avec %s ... ';
 // Text
-$_['text_title'] = 'Carte de crédit ou de débit (WorldPay)';
-$_['text_response'] = 'Réponse fournie par WorldPay : ';
-$_['text_success'] = '... votre paiement a été reçu avec succès.';
-$_['text_success_wait'] = '<b><span style="color: #FF0000">Veuillez patienter...</span></b> pendant que nous traitons votre commande.<br>Si vous n’êtes pas automatiquement redirigé dans 10 secondes, veuillez cliquer <a href="%s">ici</a>.';
-$_['text_failure'] = '... votre paiement a été annulé !';
-$_['text_failure_wait'] = '<b><span style="color: #FF0000">Veuillez patienter...</span></b><br>Si vous n’êtes pas automatiquement redirigé dans 10 secondes, veuillez cliquer <a href="%s">ici</a>.';
-$_['text_pw_mismatch'] = 'CallbackPW ne correspond pas. La commande requiert une enquête.';
+$_['text_title'] = 'Carte de crédit ou de débit (Worldpay)';
+$_['text_credit_card'] = 'Détails de la carte de crédit';
+$_['text_card_type'] = 'Type de carte';
+$_['text_card_name'] = 'Nom de la carte : ';
+$_['text_card_digits'] = 'Derniers numéros : ';
+$_['text_card_expiry'] = 'Date d’expiration : ';
+$_['text_trial'] = '%s chaque %s %s pour %s paiements alors ';
+$_['text_recurring'] = '%s chaque %s %s';
+$_['text_length'] = ' pour %s paiements';
+$_['text_confirm_delete'] = 'Êtes-vous sûr de que vouloir supprimer cette carte';
+$_['text_card_success'] = 'La carte a été supprimée avec succès.';
+$_['text_card_error'] = 'Une erreur s’est produite lors de la suppression de la carte. Veuillez contacter votre administrateur.';
+// Entry
+$_['entry_card'] = 'Nouvelle carte ou carte existante : ';
+$_['entry_card_existing'] = 'Carte existante';
+$_['entry_card_new'] = 'Nouvelle carte';
+$_['entry_card_save'] = 'Sauvegarder les détails de la carte';
+$_['entry_cc_cvc'] = 'Code de vérification de la carte (CVC)';
+$_['entry_cc_choice'] = 'Choisir une carte existante';
+// Button
+$_['button_delete_card'] = 'Détruire la carte';
+// Error
+$_['error_process_order'] = 'Une erreur s’est produite lors du traitement de votre commande. Veuillez contacter votre administrateur.';
