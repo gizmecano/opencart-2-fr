9b82e4ff8ce0856047d091e4e1b912319581cdaa
P. Mergey <gizmecano+github@gmail.com>
2017-02-16 15:20:08 +0100

    Add new files in `catalog/payment` (close #7)

---
+++ catalog/language/fr-FR/payment/globalpay.php
@@ -0,0 +1,62 @@
+<?php
+/* 
+*  Copyright (C) 2015-2016 P. Mergey
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/ 
+
+// Heading
+$_['text_title'] = 'Carte de crédit ou de débit (Globalpay)';
+
+// Button
+$_['button_confirm'] = 'Confirmer';
+
+// Entry
+$_['entry_cc_type'] = 'Type de carte';
+
+// Text
+$_['text_success'] = 'Votre paiement a été autorisé.';
+$_['text_decline'] = 'Votre paiement a été refusé.';
+$_['text_bank_error'] = 'Attention : une erreur est survenue lors du traitement de votre demande avec la banque.';
+$_['text_generic_error'] = 'Attention : une erreur est survenue lors du traitement de votre demande.';
+$_['text_hash_failed'] = 'La vérification du hachage a échoué. Ne tentez pas à nouveau d’effectuer de paiement car son statut est inconnu. Veuillez contacter l’administrateur de la boutique.';
+$_['text_link'] = 'Veuillez cliquer <a href="%s">ici</a> pour continuer';
+$_['text_select_card'] = 'Veuillez sélectionner votre type de carte';
+$_['text_result'] = 'Résultat de l’authentification';
+$_['text_message'] = 'Message';
+$_['text_cvn_result'] = 'Résultat CVN';
+$_['text_avs_postcode'] = 'Code postal AVS';
+$_['text_avs_address'] = 'Adresse AVS';
+$_['text_eci'] = 'Résultat ECI (3D secure)';
+$_['text_tss'] = 'Résultat TSS';
+$_['text_order_ref'] = 'Référence de commande';
+$_['text_timestamp'] = 'Horodatage';
+$_['text_card_type'] = 'Type de carte';
+$_['text_card_digits'] = 'Derniers numéros';
+$_['text_card_exp'] = 'Date d’expiration';
+$_['text_card_name'] = 'Nom de la carte';
+$_['text_3d_s1'] = 'Le titulaire de la carte n’est pas inscrit, transfert de responsabilité';
+$_['text_3d_s2'] = 'Impossible de vérifier l’inscription, pas de transfert de responsabilité';
+$_['text_3d_s3'] = 'Réponse invalide fournie par le serveur d’inscription, pas de transfert de responsabilité';
+$_['text_3d_s4'] = 'Inscrit, mais réponse invalide fournie par ACS (Access Control Server), pas de transfert de responsabilité';
+$_['text_3d_s5'] = 'Authentification réussie, transfert de responsabilité';
+$_['text_3d_s6'] = 'Authentification en attente de reconnaissance, transfert de responsabilité';
+$_['text_3d_s7'] = 'Saisie de mot de passe incorrect, pas de transfert de responsabilité';
+$_['text_3d_s8'] = 'Authentification indisponible, pas de transfert de responsabilité';
+$_['text_3d_s9'] = 'Réponse invalide fournie par ACS, pas de transfert de responsabilité';
+$_['text_3d_s10'] = 'Erreur fatale RealMPI, pas de transfert de responsabilité';
+$_['text_3d_liability'] = 'Pas de transfert de responsabilité';
+$_['text_card_visa'] = 'Visa';
+$_['text_card_mc'] = 'Mastercard';
+$_['text_card_amex'] = 'American Express';
+$_['text_card_switch'] = 'Switch';
+$_['text_card_laser'] = 'Laser';
+$_['text_card_diners'] = 'Diners';
---
+++ catalog/language/fr-FR/payment/globalpay_remote.php
@@ -0,0 +1,69 @@
+<?php
+/* 
+*  Copyright (C) 2015-2016 P. Mergey
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/ 
+
+// Text
+$_['text_title'] = 'Carte de crédit ou de débit';
+$_['text_credit_card'] = 'Détails de la carte de crédit';
+$_['text_wait'] = 'Veuillez patientez';
+$_['text_result'] = 'Résultat';
+$_['text_message'] = 'Message';
+$_['text_cvn_result'] = 'Résultat CVN';
+$_['text_avs_postcode'] = 'Code postal AVS';
+$_['text_avs_address'] = 'Adresse AVS';
+$_['text_eci'] = 'Résultat ECI (3D secure)';
+$_['text_tss'] = 'Résultat TSS';
+$_['text_card_bank'] = 'Banque émettrice de la carte';
+$_['text_card_country'] = 'Pays de la carte';
+$_['text_card_region'] = 'Région de la carte';
+$_['text_last_digits'] = 'Quatre derniers chiffres';
+$_['text_order_ref'] = 'Référence de commande';
+$_['text_timestamp'] = 'Horodatage';
+$_['text_card_visa'] = 'Visa';
+$_['text_card_mc'] = 'Mastercard';
+$_['text_card_amex'] = 'American Express';
+$_['text_card_switch'] = 'Switch';
+$_['text_card_laser'] = 'Laser';
+$_['text_card_diners'] = 'Diners';
+$_['text_auth_code'] = 'Code d’authentification';
+$_['text_3d_s1'] = 'Le titulaire de la carte n’est pas inscrit, transfert de responsabilité';
+$_['text_3d_s2'] = 'Impossible de vérifier l’inscription, pas de transfert de responsabilité';
+$_['text_3d_s3'] = 'Réponse invalide fournie par le serveur d’inscription, pas de transfert de responsabilité';
+$_['text_3d_s4'] = 'Inscrit, mais réponse invalide fournie par ACS (Access Control Server), pas de transfert de responsabilité';
+$_['text_3d_s5'] = 'Authentification réussie, transfert de responsabilité';
+$_['text_3d_s6'] = 'Authentification en attente de reconnaissance, transfert de responsabilité';
+$_['text_3d_s7'] = 'Saisie de mot de passe incorrect, pas de transfert de responsabilité';
+$_['text_3d_s8'] = 'Authentification indisponible, pas de transfert de responsabilité';
+$_['text_3d_s9'] = 'Réponse invalide fournie par ACS, pas de transfert de responsabilité';
+$_['text_3d_s10'] = 'Erreur fatale RealMPI, pas de transfert de responsabilité';
+
+// Entry
+$_['entry_cc_type'] = 'Type de carte';
+$_['entry_cc_number'] = 'Numéro de la carte';
+$_['entry_cc_name'] = 'Titulaire de la carte';
+$_['entry_cc_expire_date'] = 'Date d’expiration de la carte';
+$_['entry_cc_cvv2'] = 'Cryptogramme visuel';
+$_['entry_cc_issue'] = 'Numéro d’émetteur de la carte';
+
+// Help
+$_['help_start_date'] = '(si valable)';
+$_['help_issue'] = '(pour les cartes Maestro et Solo seulement)';
+
+// Error
+$_['error_card_number'] = 'Veuillez vérifier la validité du numéro de votre carte';
+$_['error_card_name'] = 'Veuillez vérifier que le nom du titulaire de la carte est valide';
+$_['error_card_cvv'] = 'Veuillez vérifier la validité du cryptogramme visuel';
+$_['error_3d_unable'] = 'La boutique requiert 3D secure mais n’est pas capable de vérifier auprès de votre banque, veuillez essayer plus tard';
+$_['error_3d_500_response_no_payment'] = 'Une réponse invalide a été reçue du processeur de carte, aucune paiement n’a été effectué';
+$_['error_3d_unsuccessful'] = 'Echec de l’autorisation 3D secure';
