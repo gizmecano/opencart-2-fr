1e46017e92cc16528d9a24d06dcf240b13c9cae4
P. Mergey <gzm@gizmecano.net>
2018-03-24 09:45:45 +0100

    Revise file in catalog/openbay folder

--- catalog/language/fr-FR/openbay/ebay_order.php
+++ catalog/language/fr-FR/openbay/ebay_order.php
@@ -19,3 +19,5 @@ $_['text_total_discount'] = 'Réduction';
 $_['text_total_tax'] = 'Taxe';
 $_['text_total_sub'] = 'Sous-total';
 $_['text_total'] = 'Total';
+$_['text_smp_id'] = 'Identifiant du gestionnaire de vente : ';
+$_['text_buyer'] = 'Nom d’utilisateur de l’acheteur : ';
