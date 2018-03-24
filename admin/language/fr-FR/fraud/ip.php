0276fcf0b16d23411e27c985d476102fc4089e0c
P. Mergey <gzm@gizmecano.net>
2018-03-24 11:25:02 +0100

    Add new file in admin/fraud folder

---
+++ admin/language/fr-FR/fraud/ip.php
@@ -0,0 +1,36 @@
+<?php
+/*
+*  Copyright (C) 2015-2016 P. Mergey
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Heading
+$_['heading_title'] = 'Antifraude IP';
+// Text
+$_['text_fraud'] = 'Antifraude';
+$_['text_success'] = 'Succès : vous avez modifié Antifraude IP!';
+$_['text_edit'] = 'Modifier Antifraude IP';
+$_['text_ip_add'] = 'Ajouter une adresse IP';
+$_['text_ip_list'] = 'Liste des adresses IP frauduleuses';
+// Column
+$_['column_ip'] = 'IP';
+$_['column_total'] = 'Comptes totaux';
+$_['column_date_added'] = 'Date d’ajout';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_ip'] = 'IP';
+$_['entry_status'] = 'Statut';
+$_['entry_order_status'] = 'Statut de commande';
+// Help
+$_['help_order_status'] = 'Les clients qui ont une adresse IP bannie sur leurs comptes se verront attribuer ce statut de commande et ne seront pas autorisés à atteindre automatiquement le statut terminé.';
+// Error
+$_['error_permission'] = 'Attention : vous n’êtes pas autorisé à modifier Antifraude IP !';
