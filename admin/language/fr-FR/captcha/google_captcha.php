9a2edafa000eda64c7e01bce54d80453e3d1bb38
P. Mergey <gzm@gizmecano.net>
2018-03-24 10:50:14 +0100

    Add new files in admin/captcha folder

---
+++ admin/language/fr-FR/captcha/google_captcha.php
@@ -0,0 +1,30 @@
+<?php
+/*
+*  Copyright (C) 2015-2016 P. Mergey
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Heading
+$_['heading_title'] = 'Google reCAPTCHA';
+// Text
+$_['text_captcha'] = 'CAPTCHA';
+$_['text_success'] = 'Succès : vous avez modifié Google reCAPTCHA!';
+$_['text_edit'] = 'Modifier Google reCAPTCHA';
+$_['text_signup'] = 'Allez sur la page <a href="https://www.google.com/recaptcha/intro/index.html" target="_blank"><u>Google reCAPTCHA</u></a> et enregistrez votre site.';
+// Entry
+$_['entry_key'] = 'Clef du site';
+$_['entry_secret'] = 'Clef secrète';
+$_['entry_status'] = 'Statut';
+// Error
+$_['error_permission'] = 'Attention : vous n’êtes pas autorisé à modifier Google reCAPTCHA!';
+$_['error_key'] = 'Clef requise !';
+$_['error_secret'] = 'Clef secrète requise !';
