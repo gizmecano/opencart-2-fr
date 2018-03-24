b1e3584af67d02ae273cfc6022c213d3726ade38
P. Mergey <gzm@gizmecano.net>
2018-03-24 10:20:02 +0100

    Revise files in catalog/api folder
    close #27

--- catalog/language/fr-FR/api/order.php
+++ catalog/language/fr-FR/api/order.php
@@ -17,11 +17,13 @@
 $_['text_success'] = 'Votre commande a été modifiée avec succès';
 // Error
 $_['error_permission'] = 'Attention : vous n’êtes pas autorisé à accéder à l’API !';
-$_['error_customer'] = 'Les détails de l’utilisateur doivent avoir été configurés';
-$_['error_payment_address'] = 'Une adresse de paiement est requise';
-$_['error_payment_method'] = 'Un moyen de paiement est requis !';
-$_['error_shipping_address'] = 'Une adresse de livraison est requise !';
-$_['error_shipping_method'] = 'Un mode de livraison est requis !';
-$_['error_stock'] = 'Les produits indiqués avec *** ne sont pas disponibles dans la quantité désirée ou ne sont actuellement pas en stock !';
-$_['error_minimum'] = 'Le montant minimum d’une commande pour %s est %s !';
+$_['error_customer'] = 'Attention : les détails de l’utilisateur doivent avoir été configurés';
+$_['error_payment_address'] = 'Attention : une adresse de paiement est requise';
+$_['error_payment_method'] = 'Attention : un moyen de paiement est requis !';
+$_['error_no_payment'] = 'Attention : aucune option de paiement n’est disponible !';
+$_['error_shipping_address'] = 'Attention : une adresse de livraison est requise !';
+$_['error_shipping_method'] = 'Attention : un mode de livraison est requis !';
+$_['error_no_shipping'] = 'Attention : aucune option d’expédition n’est disponible !';
+$_['error_stock'] = 'Attention : les produits indiqués avec *** ne sont pas disponibles dans la quantité désirée ou ne sont actuellement pas en stock !';
+$_['error_minimum'] = 'Attention : le montant minimum d’une commande pour %s est %s !';
 $_['error_not_found'] = 'Attention : la commande ne peut pas être trouvée !';
