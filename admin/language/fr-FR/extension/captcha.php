47e7c6cc851babfe0c05927b19df6603dd66a3c4
P. Mergey <gzm@gizmecano.net>
2018-03-24 11:00:42 +0100

    Add new files in admin/extension folder

---
+++ admin/language/fr-FR/extension/captcha.php
@@ -0,0 +1,26 @@
+<?php
+/*
+*  Copyright (C) 2015-2016 P. Mergey
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Heading
+$_['heading_title'] = 'CAPTCHA';
+// Text
+$_['text_success'] = 'Succès : vous avez modifié CAPTCHA !';
+$_['text_list'] = 'Liste de CAPTCHA';
+// Column
+$_['column_name'] = 'Nom de CAPTCHA';
+$_['column_status'] = 'Statut';
+$_['column_action'] = 'Action';
+// Error
+$_['error_permission'] = 'Attention : vous n’êtes pas autorisé à modifier CAPTCHA !';
