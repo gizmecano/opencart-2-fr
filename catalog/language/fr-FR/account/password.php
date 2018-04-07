46b26e8b2f859c205a1ce78a6237cfdfadad4b4b
P. Mergey <gzm@gizmecano.net>
2018-04-07 14:05:45 +0200

    Fix uppercase erroneous after colons

--- catalog/language/fr-FR/account/password.php
+++ catalog/language/fr-FR/account/password.php
@@ -18,7 +18,7 @@ $_['heading_title'] = 'Modifier le mot de passe';
 // Text
 $_['text_account'] = 'Compte';
 $_['text_password'] = 'Votre mot de passe';
-$_['text_success'] = 'Succès : Votre mot de passe a correctement été mis à jour.';
+$_['text_success'] = 'Succès : votre mot de passe a correctement été mis à jour.';
 // Entry
 $_['entry_password'] = 'Mot de passe';
 $_['entry_confirm'] = 'Confirmer ce mot de passe';
--- catalog/language/fr-FR/affiliate/success.php
+++ catalog/language/fr-FR/affiliate/success.php
@@ -16,7 +16,7 @@
 // Heading
 $_['heading_title'] = 'Votre compte d’affilié a été correctement créé !';
 // Text
-$_['text_message'] = '<p>Succès : Votre compte d’affilié a été créé avec succès !</p> <p>Vous pouvez maintenant bénéficier de tous les privilèges offerts aux membres pour améliorer votre utilisation de notre boutique en ligne.</p> <p>Si vous avez une question sur les différentes opérations possibles sur la boutique, veuillez contacter le pripriétaire par courrier électronique.</p> <p>Un message de confirmation a été envoyé à l’adresse électronique qui a été fournie. Si vous ne l’avez pas reçu dans l’heure suivante, veuillez <a href="%s">nous contacter</a>.</p>';
+$_['text_message'] = '<p>Succès : votre compte d’affilié a été créé avec succès !</p> <p>Vous pouvez maintenant bénéficier de tous les privilèges offerts aux membres pour améliorer votre utilisation de notre boutique en ligne.</p> <p>Si vous avez une question sur les différentes opérations possibles sur la boutique, veuillez contacter le pripriétaire par courrier électronique.</p> <p>Un message de confirmation a été envoyé à l’adresse électronique qui a été fournie. Si vous ne l’avez pas reçu dans l’heure suivante, veuillez <a href="%s">nous contacter</a>.</p>';
 $_['text_approval'] = '<p>Merci d’avoir enregistré un compte d’affilié sur la boutique de %s</p><p>Vous recevrez une notification par courrier électronique dès que votre compte aura été activé par le propriétaire de la boutique.</p><p>Si vous avez une question sur les opérations de ce système d’affiliation, veuillez <a href="%s">nous contacter</a>.</p>';
 $_['text_account'] = 'Compte';
 $_['text_success'] = 'Opération réussie';
