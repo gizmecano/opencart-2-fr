e755b205336b226937e11f7502501096a2ee347d
P. Mergey <gzm@gizmecano.net>
2018-04-19 10:00:18 +0200

    Add new files in catalog/credit_card folder

---
+++ catalog/language/fr-FR/credit_card/sagepay_server.php
@@ -0,0 +1,38 @@
+<?php
+/*
+*  Copyright (C) 2015-2018 P. Mergey
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Heading
+$_['heading_title'] = 'Cartes Sagepay Server';
+// Text
+$_['text_empty'] = 'Vous n’avez aucune carte enregistrée';
+$_['text_account'] = 'Compte';
+$_['text_card'] = 'Gestion de carte Sagepay Server';
+$_['text_fail_card'] = 'Un problème est survenu lors de la suppression de votre carte SagePay. Contactez l’administrateur du magasin pour obtenir de l’aide.';
+$_['text_success_card'] = 'La carte SagePay a été supprimée avec succès';
+$_['text_success_add_card'] = 'La carte SagePay a été ajoutée avec succès';
+// Column
+$_['column_type'] = 'Type de carte';
+$_['column_digits'] = 'Derniers numéros';
+$_['column_expiry'] = 'Expiration';
+// Entry
+$_['entry_cc_owner'] = 'Titulaire de la carte';
+$_['entry_cc_type'] = 'Type de carte';
+$_['entry_cc_number'] = 'Numéro de la carte';
+$_['entry_cc_expire_date'] = 'Date d’expiration de la carte';
+$_['entry_cc_cvv2'] = 'Cryptogramme visuel';
+$_['entry_cc_choice'] = 'Sélectionner une carte existante';
+// Button
+$_['button_add_card'] = 'Ajouter une carte';
+$_['button_new_card'] = 'Ajouter une nouvelle carte';
