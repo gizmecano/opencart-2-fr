e3e6bbdfd85fec797640925da9d1794677e979d7
P. Mergey <gzm@gizmecano.net>
2018-04-19 10:10:43 +0200

    Revise existing files in catalog/payment folder
    close #37

--- catalog/language/fr-FR/payment/pp_express.php
+++ catalog/language/fr-FR/payment/pp_express.php
@@ -17,7 +17,6 @@
 $_['express_text_title'] = 'Confirmer la commande';
 // Text
 $_['text_title'] = 'Paiement Express via PayPal';
-$_['button_continue'] = 'Continuer';
 $_['text_cart'] = 'Panier d’achat';
 $_['text_shipping_updated'] = 'Mode de livraison mis à jour';
 $_['text_trial'] = '%s chaque %s %s pour %s paiements soit ';
@@ -31,7 +30,6 @@ $_['express_button_coupon'] = 'Ajouter';
 $_['express_button_confirm'] = 'Confirmer';
 $_['express_button_login'] = 'Continuer sur PayPal';
 $_['express_button_shipping'] = 'Mettre à jour la livraison';
-$_['button_cancel_recurring'] = 'Annuler les paiements';
 // Error
 $_['error_heading_title'] = 'Une erreur s’est produite';
 $_['error_too_many_failures'] = 'Votre paiement a échoué trop souvent';
--- catalog/language/fr-FR/payment/sagepay_direct.php
+++ catalog/language/fr-FR/payment/sagepay_direct.php
@@ -23,6 +23,8 @@ $_['text_card_expiry'] = 'Date d’expiration';
 $_['text_trial'] = '%s chaque %s %s pour %s paiements soit ';
 $_['text_recurring'] = '%s chaque %s %s';
 $_['text_length'] = ' pour %s paiements';
+$_['text_fail_card'] = 'Un problème est survenu lors de la suppression de votre carte SagePay. Contactez l’administrateur du magasin pour obtenir de l’aide.';
+$_['text_confirm_delete'] = 'Êtes-vous certain de que vouloir supprimer cette carte';
 // Entry
 $_['entry_card'] = 'Nouvelle carte ou carte existante : ';
 $_['entry_card_existing'] = 'Existant';
@@ -31,11 +33,8 @@ $_['entry_card_save'] = 'Se souvenir des détails de la carte';
 $_['entry_cc_owner'] = 'Titulaire de la carte';
 $_['entry_cc_type'] = 'Type de carte';
 $_['entry_cc_number'] = 'Numéro de la carte';
-$_['entry_cc_start_date'] = 'Carte valable depuis la date';
 $_['entry_cc_expire_date'] = 'Date d’expiration de la carte';
 $_['entry_cc_cvv2'] = 'Cryptogramme visuel';
-$_['entry_cc_issue'] = 'Numéro d’émission de la carte';
 $_['entry_cc_choice'] = 'Sélectionner une carte existante';
-// Help
-$_['help_start_date'] = '(si valable)';
-$_['help_issue'] = '(pour les cartes Maestro et Solo seulement)';
+// Button
+$_['button_delete_card'] = 'Supprimer la carte sélectionnée';
