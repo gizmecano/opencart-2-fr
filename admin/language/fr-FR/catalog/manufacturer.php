98bbc1aac6dd3b287b64151888472bdfe74b7f22
P. Mergey <gzm@gizmecano.net>
2018-04-23 09:20:23 +0200

    Revise existing files in admin/catalog folder

--- admin/language/fr-FR/catalog/manufacturer.php
+++ admin/language/fr-FR/catalog/manufacturer.php
@@ -30,14 +30,14 @@ $_['column_action'] = 'Action';
 // Entry
 $_['entry_name'] = 'Nom de la marque';
 $_['entry_store'] = 'Boutiques';
-$_['entry_keyword'] = 'Mot-clef de référencement';
+$_['entry_keyword'] = 'URL de référencement';
 $_['entry_image'] = 'Image';
 $_['entry_sort_order'] = 'Classement';
 $_['entry_type'] = 'Type';
 // Help
-$_['help_keyword'] = 'N’utilisez pas d’espace mais des tirets à leur place et faites en sorte que le mot-clef soit le plus spécifique possible.';
+$_['help_keyword'] = 'N’utilisez pas d’espace mais des tirets à leur place et faites en sorte que l’URL soit le plus spécifique possible.';
 // Error
 $_['error_permission'] = 'Attention : vous n’êtes pas autorisé à modifier les marques !';
 $_['error_name'] = 'La composition du nom du fabricant doit être comprise entre de 2 à 64 caractères !';
-$_['error_keyword'] = 'Mot-clef de référencement (SEO) déjà en cours d’utilisation !';
+$_['error_keyword'] = 'URL de référencement (SEO) déjà en cours d’utilisation !';
 $_['error_product'] = 'Attention : ce fabricant ne peut être supprimé car il est actuellement affecté aux produits %s !';
