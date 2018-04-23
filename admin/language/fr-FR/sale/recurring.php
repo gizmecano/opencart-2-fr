0e756c0a66857dc46a83153c1b2a48f6bcff353c
P. Mergey <gzm@gizmecano.net>
2018-04-23 10:30:15 +0200

    Revise existing file in admin/sale folder

--- admin/language/fr-FR/sale/recurring.php
+++ admin/language/fr-FR/sale/recurring.php
@@ -14,21 +14,39 @@
 */ 
 
 // Heading
-$_['heading_title'] = 'Profils récurrents';
+$_['heading_title'] = 'Commandes récurrentes';
 // Text
-$_['text_success'] = 'Vous venez de modifier le(s) profil(s) récurrent(s) !';
-$_['text_list'] = 'Liste des profils récurrents';
-$_['text_add'] = 'Ajouter un profil récurrent';
-$_['text_edit'] = 'Modifier un profil récurrent';
-$_['text_payment_profiles'] = 'Profils récurrents';
-$_['text_status_active'] = 'Activé';
-$_['text_status_inactive'] = 'Désactivé';
-$_['text_status_cancelled'] = 'Annulé';
-$_['text_status_suspended'] = 'Suspendu';
-$_['text_status_expired'] = 'Expiré';
-$_['text_status_pending'] = 'En attente';
+$_['text_success'] = 'Vous venez de modifier les commandes récurrente !';
+$_['text_list'] = 'Liste des commandes récurrentes';
+$_['text_recurring_detail'] = 'Détails de commande récurrente';
+$_['text_order_detail'] = 'Détails de la commande';
+$_['text_product_detail'] = 'Détails de produit';
+$_['text_transaction'] = 'Transactions';
+$_['text_order_recurring_id'] = 'Identifiant de commande récurrente';
+$_['text_reference'] = 'Référence de paiement';
+$_['text_recurring_name'] = 'Profil de la commande récurrente';
+$_['text_recurring_description'] = 'Description de la commande récurrente';
+$_['text_recurring_status'] = 'Statut de la commande récurrente';
+$_['text_payment_method'] = 'Moyen de paiement';
+$_['text_order_id'] = 'Commande n°';
+$_['text_customer'] = 'Client';
+$_['text_email'] = 'Adresse électronique';
+$_['text_date_added'] = 'Date d’ajout';
+$_['text_order_status'] = 'Statut de la commande';
+$_['text_type'] = 'Type';
+$_['text_action'] = 'Action';
+$_['text_product'] = 'Produit';
+$_['text_quantity'] = 'Quantité';
+$_['text_amount'] = 'Montant';
+$_['text_cancel_payment'] = 'Annulation de paiement';
+$_['text_status_1'] = 'Activée';
+$_['text_status_2'] = 'Inactif';
+$_['text_status_3'] = 'Annulé';
+$_['text_status_4'] = 'Suspendu';
+$_['text_status_5'] = 'Expiré';
+$_['text_status_6'] = 'En attente';
 $_['text_transactions'] = 'Transactions';
-$_['text_cancel_confirm'] = 'L’annulation de profil ne peut pas être annulée! Êtes-vous certain de vouloir le faire ?';
+$_['text_cancel_confirm'] = 'L’annulation de commande récurrente ne peut pas être annulée ! Êtes-vous certain de vouloir le faire ?';
 $_['text_transaction_date_added'] = 'Date d’ajout';
 $_['text_transaction_payment'] = 'Paiement';
 $_['text_transaction_outstanding_payment'] = 'Paiement en suspens';
@@ -39,9 +57,19 @@ $_['text_transaction_suspended'] = 'Suspendu';
 $_['text_transaction_suspended_failed'] = 'Suspendu en raison d’un échec du paiement';
 $_['text_transaction_outstanding_failed'] = 'Paiement en suspens échoué';
 $_['text_transaction_expired'] = 'Expiré';
+$_['text_cancelled'] = 'Le paiement récurrent a été annulé';
+// Column
+$_['column_order_recurring_id'] = 'Identifiant de commande récurrente';
+$_['column_order_id'] = 'Commande n°';
+$_['column_reference'] = 'Référence de paiement';
+$_['column_customer'] = 'Client';
+$_['column_date_added'] = 'Date d’ajout';
+$_['column_status'] = 'Statut';
+$_['column_amount'] = 'Montant';
+$_['column_type'] = 'Type';
+$_['column_action'] = 'Action';
 // Entry
-$_['entry_cancel_payment'] = 'Annuler le paiement';
-$_['entry_order_recurring'] = 'ID';
+$_['entry_order_recurring_id'] = 'Identifiant de commande récurrente';
 $_['entry_order_id'] = 'Commande n°';
 $_['entry_reference'] = 'Référencement de paiement';
 $_['entry_customer'] = 'Client';
@@ -56,7 +84,7 @@ $_['entry_quantity'] = 'Quantité';
 $_['entry_amount'] = 'Montant';
 $_['entry_recurring'] = 'Profil récurrent';
 $_['entry_payment_method'] = 'Moyen de paiement';
-// Error / Success
+$_['entry_cancel_payment'] = 'Annuler le paiement';
+// Error
 $_['error_not_cancelled'] = 'Erreur : %s';
 $_['error_not_found'] = 'Le profil récurrent ne peut être annulé';
-$_['text_cancelled'] = 'Le paiement récurrent a été annulé';
