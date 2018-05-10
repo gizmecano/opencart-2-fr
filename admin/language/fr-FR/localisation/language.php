430195b62249a83f3e0c8fe31a06d2fc1079b179
P. Mergey <gzm@gizmecano.net>
2018-05-10 17:25:03 +0200

    Revise existing file in admin/localisation folder

--- admin/language/fr-FR/localisation/language.php
+++ admin/language/fr-FR/localisation/language.php
@@ -28,24 +28,15 @@ $_['column_action'] = 'Action';
 // Entry
 $_['entry_name'] = 'Nom de la langue';
 $_['entry_code'] = 'Code';
-$_['entry_locale'] = 'Localisation';
-$_['entry_image'] = 'Image';
-$_['entry_directory'] = 'Dossier';
 $_['entry_status'] = 'Statut';
 $_['entry_sort_order'] = 'Classement';
 // Help
-$_['help_code'] = 'Exemple : « fr ». Ne pas changer s’il s’agit de votre langue par défaut.';
-$_['help_locale'] = 'Exemple : « en_US.UTF-8 » ou « fr_FR » ou « fr-fr » ou « en_gb » ou « french »';
-$_['help_image'] = 'Exemple : « fr.png »';
-$_['help_directory'] = 'Nom du dossier de langue (sensible à la casse)';
 $_['help_status'] = 'Masquer/Afficher dans le menu déroulant des langues';
 // Error
 $_['error_permission'] = 'Attention : vous n’êtes pas autorisé à modifier les langues !';
+$_['error_exists'] = 'Attention : vous avez déjà ajouté cette langue !';
 $_['error_name'] = 'Le nom de la langue doit contenir entre 3 et 32 caractères !';
 $_['error_code'] = 'Le code de la langue doit être d’au moins 2 caractères !';
-$_['error_locale'] = 'Le paramètre « région » est requis !';
-$_['error_image'] = 'Le nom du fichier image doit contenir de 3 à 64 caractères !';
-$_['error_directory'] = 'Vous devez choisir un dossier langue !';
 $_['error_default'] = 'Attention : cette langue ne peut être supprimée car elle est actuellement assignée comme langue par défaut !';
 $_['error_admin'] = 'Attention : cette langue ne peut être supprimée car elle est actuellement assignée comme langue du panneau d’administration !';
 $_['error_store'] = 'Attention : cette langue ne peut être supprimée car elle est actuellement attribuée à %s boutiques !';
