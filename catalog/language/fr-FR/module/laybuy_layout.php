e906acd06b0a3f8c6591f5b02989103cb1cd1a2c
P. Mergey <gzm@gizmecano.net>
2018-05-10 18:00:11 +0200

    Add new file in catalog/module folder

---
+++ catalog/language/fr-FR/module/laybuy_layout.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ *  Copyright (C) 2015-2018 P. Mergey
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Heading
+$_['heading_title'] = 'Information Lay-Buy';
+// Text
+$_['text_reference_info'] = 'Informations de référence';
+$_['text_laybuy_ref_no'] = 'Identifiant de référence Lay-Buy :';
+$_['text_paypal_profile_id'] = 'Identifiant de profil PayPal :';
+$_['text_payment_plan'] = 'Plan de paiement :';
+$_['text_status'] = 'Statut :';
+$_['text_amount'] = 'Montant :';
+$_['text_downpayment_percent'] = 'Pourcentage du premier versement :';
+$_['text_months'] = 'Mois :';
+$_['text_downpayment_amount'] = 'Montant du premier versement :';
+$_['text_payment_amounts'] = 'Montants du paiement :';
+$_['text_first_payment_due'] = 'Premier paiement dû :';
+$_['text_last_payment_due'] = 'Dernier paiement dû :';
+$_['text_downpayment'] = 'Premier versement :';
+$_['text_month'] = 'Mois :';
+// Column
+$_['column_instalment'] = 'Versement';
+$_['column_amount'] = 'Montant';
+$_['column_date'] = 'Date';
+$_['column_pp_trans_id'] = 'Identifiant de transaction PayPal';
+$_['column_status'] = 'Statut';
