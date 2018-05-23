157d3ea10b62135ece0284933f6842f37ef33734
P. Mergey <gzm@gizmecano.net>
2018-05-23 11:35:05 +0200

    Add new files in catalog/payment folder
    close #34

---
+++ catalog/language/fr-FR/payment/laybuy.php
@@ -0,0 +1,40 @@
+<?php
+/*
+*  Copyright (C) 2015-2018 P. Mergey
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Heading
+$_['heading_title'] = 'Veuillez choisir votre plan de paiement';
+// Text
+$_['text_title'] = 'PUT IT ON LAY-BUY powered by PayPal';
+$_['text_plan_preview'] = 'Aperçu du plan de paiement';
+$_['text_payment'] = 'Paiement';
+$_['text_due_date'] = 'Date d’échéance';
+$_['text_amount'] = 'Montant';
+$_['text_downpayment'] = 'Premier versement';
+$_['text_today'] = 'Aujourd’hui';
+$_['text_delivery_msg'] = 'Vos produits ou services seront livrés une fois reçu votre paiement final.';
+$_['text_fee_msg'] = 'Des frais d’administration de 0,9 % sont payables à Lay-Buy.';
+$_['text_month'] = 'Mois';
+$_['text_months'] = 'Mois';
+$_['text_status_1'] = 'En attente';
+$_['text_status_5'] = 'Terminé';
+$_['text_status_7'] = 'Annulé';
+$_['text_status_50'] = 'Modification demandée';
+$_['text_status_51'] = 'Modifié';
+$_['text_comment'] = 'Mis à jour par Lay-Buy';
+// Entry
+$_['entry_initial'] = 'Paiement initial';
+$_['entry_months'] = 'Mois';
+// Button
+$_['button_confirm'] = 'Confirmer';
