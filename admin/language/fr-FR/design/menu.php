4500f742cb289a2a7e411edcc4536cea98633b23
P. Mergey <gzm@gizmecano.net>
2018-06-01 16:15:15 +0200

    Add new files in admin/design folder

---
+++ admin/language/fr-FR/design/menu.php
@@ -0,0 +1,44 @@
+<?php
+/* 
+*  Copyright (C) 2015-2018 P. Mergey
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/ 
+
+// Heading
+$_['heading_title'] = 'Menu';
+// Text
+$_['text_success'] = 'Vous venez de modifier les gabarits !';
+$_['text_list'] = 'Liste de menu';
+$_['text_add'] = 'Ajouter un menu';
+$_['text_edit'] = 'Modifier un menu';
+$_['text_default'] = 'Par défaut';
+$_['text_link'] = 'Lien';
+$_['text_module'] = 'Module';
+// Column
+$_['column_name'] = 'Nom du menu';
+$_['column_store'] = 'Boutique';
+$_['column_type'] = 'Type';
+$_['column_sort_order'] = 'Classement';
+$_['column_status'] = 'Statut';
+$_['column_action'] = 'Action';
+// Entry
+$_['entry_name'] = 'Nom du menu';
+$_['entry_store'] = 'Boutique';
+$_['entry_type'] = 'Type';
+$_['entry_sort_order'] = 'Classement';
+$_['entry_module'] = 'Module';
+$_['entry_status'] = 'Statut';
+// Error
+$_['error_permission'] = 'Attention : vous n’êtes pas autorisé à modifier les gabarits !';
+$_['error_name'] = 'Le nom du gabarit doit contenir entre 3 et 64 caractères !';
+$_['error_default'] = 'Attention : ce gabarit ne peut être supprimée car il est actuellement défini comme gabarit par défaut pour cette boutique !';
+$_['error_store'] = 'Attention : ce gabarit ne peut être supprimé car il est actuellement attribué à %s boutiques !';
---
+++ admin/language/fr-FR/design/theme.php
@@ -0,0 +1,29 @@
+<?php
+/*
+*  Copyright (C) 2015-2018 P. Mergey
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Heading
+$_['heading_title'] = 'Éditeur de thème';
+// Text
+$_['text_success'] = 'Vous venez de modifier les thèmes !';
+$_['text_edit'] = 'Modifier le thème';
+$_['text_store'] = 'Choisir la boutique';
+$_['text_template'] = 'Choisir le modèle';
+$_['text_default'] = 'Par défaut';
+$_['text_warning']= 'Attention : la sécurité peut être compromise par l’utilisation de l’éditeur de thème !';
+$_['text_access'] = 'Assurez-vous que seuls les administrateurs sont autorisés à accéder à cette page, car il est possible d’accéder directement au code source de votre boutique.';
+$_['text_permission'] = 'Vous pouvez changer les permissions de l’utilisateur <a href="%s" class="alert-link">ici</a>.';
+$_['text_begin'] = 'Sélectionnez un fichier de thème sur le côté gauche pour commencer l’édition.';
+// Error
+$_['error_permission'] = 'Attention : vous n’êtes pas autorisé à modifier l’éditeur de thème !';
