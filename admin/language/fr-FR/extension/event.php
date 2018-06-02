c12f40f2f879807c0dc33331fc75c4ee559cccaf
P. Mergey <gzm@gizmecano.net>
2018-06-02 09:25:08 +0200

    Add new files in admin/extension folder

---
+++ admin/language/fr-FR/extension/event.php
@@ -0,0 +1,30 @@
+<?php
+/*
+*  Copyright (C) 2015-2018 P. Mergey
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Heading
+$_['heading_title'] = 'Événements';
+// Text
+$_['text_success'] = 'Vous venez de modifier les événements !';
+$_['text_list'] = 'Liste d’événements';
+$_['text_event'] = 'Les événements sont utilisés par les extensions pour remplacer les fonctionnalités par défaut de votre boutique. Si vous rencontrez des problèmes, vous pouvez activer ou désactiver les événements ici.';
+// Column
+$_['column_code'] = 'Code de l’événement';
+$_['column_trigger'] = 'Déclencheur';
+$_['column_action'] = 'Action';
+$_['column_status'] = 'Statut';
+$_['column_date_added'] = 'Date d’ajout';
+$_['column_action'] = 'Action';
+// Error
+$_['error_permission'] = 'Attention : vous n’êtes pas autorisé à modifier les événements !';
