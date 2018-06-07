bd3f797a7db36de28f4d99d616cef2a090ed920a
P. Mergey <gzm@gizmecano.net>
2018-06-07 07:35:05 +0200

    Add new files in catalog/extension/payment folder

---
+++ catalog/language/fr-FR/extension/payment/cardconnect.php
@@ -0,0 +1,55 @@
+<?php
+/*
+*  Copyright (C) 2015-2018 P. Mergey
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Text
+$_['text_title'] = 'Carte de crédit ou de débit';
+$_['text_card_details'] = 'Détails de la carte';
+$_['text_echeck_details'] = 'Détails du eChèque (eCheck)';
+$_['text_card'] = 'Carte';
+$_['text_echeck'] = 'eChèque (eCheck)';
+$_['text_wait'] = 'Veuillez patienter !';
+$_['text_confirm_delete'] = 'Êtes vous certain de vouloir supprimer la carte ?';
+$_['text_no_cards'] = 'Aucune carte enregistrée actuellement';
+$_['text_select_card'] = 'Veuillez sélectionner une carte';
+// Entry
+$_['entry_method'] = 'Méthode';
+$_['entry_card_new_or_old'] = 'Nouvelle carte ou carte existante';
+$_['entry_card_new'] = 'Nouvelle carte';
+$_['entry_card_old'] = 'Carte existante';
+$_['entry_card_type'] = 'Type de carte';
+$_['entry_card_number'] = 'Numéro de la carte';
+$_['entry_card_expiry'] = 'Date d’expiration';
+$_['entry_card_cvv2'] = 'Cryptogramme visuel';
+$_['entry_card_save'] = 'Enregistrer la carte';
+$_['entry_card_choice'] = 'Choisir votre carte';
+$_['entry_account_number'] = 'Numéro de compte';
+$_['entry_routing_number'] = 'Numéro de routage';
+// Button
+$_['button_confirm'] = 'Confirmer la commande';
+$_['button_delete'] = 'Supprimer la carte sélectionnée';
+// Error
+$_['error_card_number'] = 'Le numéro de la carte doit contenir entre 1 et 19 caractères !';
+$_['error_card_type'] = 'Le type de carte n’est pas une sélection valide!';
+$_['error_card_cvv2'] = 'Le cryptogramme visuel doit contenir entre 1 et 4 caractères !';
+$_['error_data_missing'] = 'Données manquantes !';
+$_['error_not_logged_in'] = 'Pas connecté !';
+$_['error_no_order'] = 'Aucune commande correspondante !';
+$_['error_no_post_data'] = 'Pas de données $_POST';
+$_['error_select_card'] = 'Veuillez sélectionner une carte!';
+$_['error_no_card'] = 'Aucune carte trouvée !';
+$_['error_no_echeck'] = 'eChèque (eCheck) n’est pas supporté !';
+$_['error_account_number'] = 'Numéro de compte doit contenir entre 1 et 19 caractères !';
+$_['error_routing_number'] = 'Numéro de routage doit contenir entre 1 et 9 caractères !';
+$_['error_not_enabled'] = 'Le module n’est pas activé !';
---
+++ catalog/language/fr-FR/extension/payment/klarna_checkout.php
@@ -0,0 +1,25 @@
+<?php
+/*
+*  Copyright (C) 2015-2018 P. Mergey
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Heading
+$_['heading_title'] = 'Klarna Checkout';
+$_['heading_title_success'] = 'Votre commande Klarna Checkout a été prise en compte!';
+// Text
+$_['text_title'] = 'Klarna Checkout';
+$_['text_basket'] = 'Panier d’achat';
+$_['text_checkout'] = 'Valider la commande';
+$_['text_success'] = 'Succès';
+$_['text_choose_shipping_method'] = 'Choisir un mode de livraison';
+$_['text_sales_tax'] = 'Taxe sur les ventes';
