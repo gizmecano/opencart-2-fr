8271f70c14429c2887bc76a8ec856b5f39257a87
P. Mergey <gzm@gizmecano.net>
2018-06-07 08:50:03 +0200

    Add new files in admin/extension/module folder

---
+++ admin/language/fr-FR/extension/module/divido_calculator.php
@@ -0,0 +1,25 @@
+<?php
+/*
+*  Copyright (C) 2015-2018 P. Mergey
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Heading
+$_['heading_title'] = 'Calculateur de page de produit Divido';
+// Text
+$_['text_extension'] = 'Extensions';
+$_['text_success'] = 'Vous venez de modifier le calculateur de page de produit Divido !';
+$_['text_edit'] = 'Modifier le calculateur de page de produit Divido';
+// Entry
+$_['entry_status'] = 'Statut';
+// Error
+$_['error_permission'] = 'Attention : vous n’êtes pas autorisé à modifier le calculateur de page de produit Divido!';
